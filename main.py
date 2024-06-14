0b04f62f753b5880ed35bc95029988f9f4dfeeff
carrot729 <hnf729@outlook.com>
2024-06-15 00:25:07 +0800

    0

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from simple_replace import simple_replace,rotors
+from simple_replace import simple_replace, rotors
 
 
 def main():
@@ -21,19 +21,20 @@ def main():
     rotor1 = input("Enter rotor 1: ")
     rotor2 = input("Enter rotor 2: ")
     rotor3 = input("Enter rotor 3: ")
-    rotor1 = ord(rotor1)-ord('A')
-    rotor2 = ord(rotor2)-ord('A')
-    rotor3 = ord(rotor3)-ord('A')
+    rotor1 = ord(rotor1) - ord('A')
+    rotor2 = ord(rotor2) - ord('A')
+    rotor3 = ord(rotor3) - ord('A')
     for i in range(rotor1):
         rotors(r_word1)
     for i in range(rotor2):
         rotors(r_word2)
     for i in range(rotor3):
         rotors(r_word3)
-    print('Running:',end='')
-    new_pass=simple_replace(password,r_word1,r_word2,r_word3)
-    print('New password:',end='')
+    print('Running:', end='')
+    new_pass = simple_replace(password, r_word1, r_word2, r_word3)
+    print('New password:', end='')
     print(new_pass)
+
 
 if __name__ == '__main__':
     main()
